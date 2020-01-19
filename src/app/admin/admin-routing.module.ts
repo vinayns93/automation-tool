0428da740ce3e4734d35d7e8de7727e589e11ba3
VishnuBalasubramonian <vishnu.balasubramonian@outlook.com>
2020-01-19 19:06:17 +0530

    WIP

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestDataComponent, TestScriptsComponent, RepositoryComponent, KeywordsComponent, FeatureComponent } from './pages';
 import { AddKeywordComponent } from './pages/keywords/add-keyword/add-keyword.component';
+import { EditKeywordComponent } from './pages/keywords/edit-keyword/edit-keyword.component';
 
 
 
@@ -12,6 +13,7 @@ export const adminRoutes: Routes = [
       { path: 'admin/keywords', component: KeywordsComponent },
       { path: 'admin/feature', component: FeatureComponent },
       { path: 'admin/keywords/add', component: AddKeywordComponent},
+      { path: 'admin/keywords/edit/:id/:userId', component: EditKeywordComponent },
       { path: '**', redirectTo: 'admin/testscripts', pathMatch: 'full' }
 ];
 
---
+++ src/app/admin/pages/keywords/edit-keyword/edit-keyword.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Keywords } from '../../../../core/models';
+import { ActivatedRoute } from '@angular/router';
+import { KeywordService } from '../../../../services/keyword.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-keyword',
+  templateUrl: './edit-keyword.component.html',
+  styleUrls: ['./edit-keyword.component.scss']
+})
+export class EditKeywordComponent implements OnInit {
+
+  editKeywordObj: Keywords;
+  id: any;
+  userId: any;
+  keywordsForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    functionName:  new FormControl('', Validators.required),
+    stepDescription:  new FormControl('', Validators.required),
+    actionOrKeyword: new FormControl('', Validators.required),
+    objectLogicalName: new FormControl('', Validators.required),
+    run: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]),
+    param1: new FormControl('', [Validators.required]),
+    param2: new FormControl('', Validators.required),
+    param3: new FormControl('', Validators.required),
+    param4: new FormControl('', Validators.required),
+    param5: new FormControl('', Validators.required),
+    param6: new FormControl('', Validators.required),
+    param7: new FormControl('', Validators.required),
+    param8: new FormControl('', Validators.required),
+    param9: new FormControl('', Validators.required),
+    param10: new FormControl('', Validators.required),
+    module: new FormControl('', Validators.required),
+    statusID: new FormControl('', Validators.required),
+    cudStatusID: new FormControl('', Validators.required),
+    isLocked: new FormControl('', Validators.required)
+});
+
+  constructor(private activatedRoute: ActivatedRoute, private service: KeywordService) {
+    this.id = activatedRoute.snapshot.paramMap.get("id");
+    this.userId = activatedRoute.snapshot.paramMap.get("id");
+   }
+
+  ngOnInit() {
+    let getKeywordreq = this.service.getKeyword(this.id, this.userId)
+    .subscribe((result: Keywords) => {
+      this.editKeywordObj = result;
+      getKeywordreq.unsubscribe()
+    },
+    error => {
+      console.log(error);
+    },
+    () => {
+
+    });
+  }
+
+}
